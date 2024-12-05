700b4ba08797ff8d5f1b7aaa960bb4ee70ea0fc0
loek08 <558777@student.fontys.nl>
2024-12-05 13:53:06 +0100

    buttons in een menu list

--- lib/home.dart
+++ lib/home.dart
@@ -8,62 +8,68 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
+        title: Text(
+          "Home",
+          style: TextStyle(color: Colors.white, fontSize: 24),
+        ),
+        centerTitle: true,
       ),
-      body: FutureBuilder<Map<String, Map<String, dynamic>>>(
-        future: Future.value(barcode.getProductDatabase()),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
-
-          if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text("Geen producten gevonden"));
-          }
-
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/barcode');
-                  },
-                  child: Text("Voeg producten toe"),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/barcode2');
-                  },
-                  child: Text("Verwijder producten"),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/addunknownbarcode');
-                  },
-                  child: Text("add unkown barcode"),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/deleteknownbarcode');
-                  },
-                  child: Text("delete known barcode"),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/list');
-                  },
-                  child: Text("barcode list"),
-                )
-              ],
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 243, 33, 33),
+              ),
+              child: Text(
+                'menu',
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
             ),
-          );
-        },
+            ListTile(
+              leading: Icon(Icons.add),
+              title: Text('Voeg producten toe'),
+              onTap: () {
+                Navigator.pushNamed(context, '/barcode');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.delete),
+              title: Text('Verwijder producten'),
+              onTap: () {
+                Navigator.pushNamed(context, '/barcode2');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.add),
+              title: Text('add unkown barcode'),
+              onTap: () {
+                Navigator.pushNamed(context, '/addunknownbarcode');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.delete),
+              title: Text('Delete known barcodes'),
+              onTap: () {
+                Navigator.pushNamed(context, '/deleteknownbarcode');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.list),
+              title: Text('Product lijst'),
+              onTap: () {
+                Navigator.pushNamed(context, '/list');
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Text(
+          'Welkom bij de Barcode App!',
+          style: TextStyle(fontSize: 24),
+        ),
       ),
     );
   }
